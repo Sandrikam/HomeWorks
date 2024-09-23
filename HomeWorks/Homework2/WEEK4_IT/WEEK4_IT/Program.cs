a68ada5ef7dfe064cc7630677330ad04b5d4b475
sandrikam <a.maisuradze@silkbank.ge>
2024-09-23 15:35:56 +0400

    Added Console Application

---
+++ HomeWorks/Homework2/WEEK4_IT/WEEK4_IT/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WEEK4_IT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Sandro Maisuradze");
+            Console.WriteLine(Console.ReadLine());
+
+        }
+    }
+}
